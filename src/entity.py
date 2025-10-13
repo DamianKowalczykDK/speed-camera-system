7c732451d0d78b876addf8d5ca110cd910f98ea2
Damian <damiankowalczyk3688@gmail.com>
2025-10-14 01:33:25 +0200

    Add base Entity class with id_ attribute

--- src/entity.py
+++ src/entity.py
@@ -1,29 +1,81 @@
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from typing import Self, override
+
 
 @dataclass
-class SpeedCamera:
+class Entity(ABC):
     id_: int | None = None
+
+    @classmethod
+    @abstractmethod
+    def from_row(cls, *args: str) -> Self: # pragma: no cover
+        pass
+
+
+@dataclass
+class SpeedCamera(Entity):
     location: str | None = None
     allowed_speed: int | None = None
 
+    @classmethod
+    @override
+    def from_row(cls, *args: str) -> Self:
+        return cls(
+            id_=int(args[0]),
+            location=str(args[1]),
+            allowed_speed=int(args[2]),
+        )
+
+
 @dataclass
-class Driver:
-    id_: int | None = None
+class Driver(Entity):
     first_name: str | None = None
     last_name: str | None = None
     registration_number: str | None = None
 
+    @classmethod
+    @override
+    def from_row(cls, *args: str) -> Self:
+        return cls(
+            id_=int(args[0]),
+            first_name=str(args[1]),
+            last_name=str(args[2]),
+            registration_number=str(args[3])
+        )
+
+
 @dataclass
-class Offence:
-    id_: int | None = None
+class Offense(Entity):
     description: str | None = None
     penalty_points: int | None = None
     fine_amount: int | None = None
 
+    @classmethod
+    @override
+    def from_row(cls, *args: str) -> Self:
+        return cls(
+            id_=int(args[0]),
+            description=str(args[1]),
+            penalty_points=int(args[2]),
+            fine_amount=int(args[3]),
+        )
+
+
 @dataclass
-class Violation:
-    id_: int | None = None
+class Violation(Entity):
     violation_date: str | None = None
     driver_id: int | None = None
     speed_camera_id: int | None = None
-    offence_id: int | None = None
+    offense_id: int | None = None
+
+    @classmethod
+    @override
+    def from_row(cls, *args: str) -> Self:
+        return cls(
+            id_=int(args[0]),
+            violation_date=str(args[1]),
+            driver_id=int(args[2]),
+            speed_camera_id=int(args[3]),
+            offense_id=int(args[4]),
+        )
