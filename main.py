3e826ed84348d9ff84c99770367aa2ba627d8641
Damian <damiankowalczyk3688@gmail.com>
2025-10-16 01:44:33 +0200

    add main.py and test_repository.py

---
+++ main.py
@@ -0,0 +1,59 @@
+from src.entity import Driver, SpeedCamera, Offense, Violation
+from src.database import MySQLConnectionManager
+from src.execute_sql_file import SqlFileExecutor
+from src.repository import (
+    DriverRepository,
+    SpeedCameraRepository,
+    ViolationRepository,
+    OffenseRepository
+)
+from src.service import ViolationService
+
+
+def main() -> None:
+
+    mysql_connection_manager = MySQLConnectionManager()
+
+
+    # sql_executor = SqlFileExecutor(mysql_connection_manager)
+    # sql_executor.execute_sql_file('sql/schema.sql')
+    # sql_executor.execute_sql_file('sql/data.sql')
+
+    driver_repository = DriverRepository(mysql_connection_manager)
+    offense_repository = OffenseRepository(mysql_connection_manager)
+    speed_camera_repository = SpeedCameraRepository(mysql_connection_manager)
+    violation_repository = ViolationRepository(mysql_connection_manager)
+
+    service = ViolationService(driver_repository, speed_camera_repository, offense_repository, violation_repository)
+    # service.add_driver(Driver(first_name='x', last_name='y', registration_number='xxx'))
+    # print(service.all_offenses_by_driver('xxxxxx'))
+    print(service.get_top_drivers_by_points())
+
+    # speed_cameras = speed_camera_repository.find_all()
+    # for speed_camera in speed_cameras:
+    #     print(speed_camera)
+
+    # print(speed_camera_repository.find_by_id(1))
+    # print(speed_camera_repository.delete(5))
+
+    # speed_camera_repository.update(2, SpeedCamera(location='aaa', allowed_speed=-1))
+    # speed_camera_repository.insert_many([
+    #     SpeedCamera(location='XXX', allowed_speed=220),
+    #     SpeedCamera(location='YYY', allowed_speed=230)
+    # ])
+
+    # speed_camera_repository.insert(SpeedCamera(location='ZZZ', allowed_speed=230))
+
+    #
+    # driver_repository = DriverRepository(mysql_connection_manager)
+    # print(driver_repository.find_all())
+    #
+    # offense_repository = OffenseRepository(mysql_connection_manager)
+    # print(offense_repository.find_all())
+    #
+    # violation_repository = ViolationRepository(mysql_connection_manager)
+    # print(violation_repository.find_all())
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_repository.py
@@ -0,0 +1,113 @@
+from src.repository import DriverRepository, SpeedCameraRepository, ViolationRepository, OffenseRepository
+from src.entity import Driver, SpeedCamera, Offense, Violation
+from src.execute_sql_file import SqlFileExecutor
+from src.database import MySQLConnectionManager
+from mysql.connector import Error
+import pytest
+import os
+
+
+def test_insert_driver(driver_repository: DriverRepository, driver_1: Driver) -> None:
+    driver_id = driver_repository.insert(driver_1)
+    assert driver_id is not None
+
+    retrieved_driver = driver_repository.find_by_id(1)
+    assert retrieved_driver.first_name == driver_1.first_name
+    assert retrieved_driver.last_name == driver_1.last_name
+    assert retrieved_driver.registration_number == driver_1.registration_number
+
+def test_insert_driver_and_find_all(driver_repository: OffenseRepository, driver_1: Driver, clear_database) -> None:
+    driver_id = driver_repository.insert(driver_1)
+    assert driver_id is not None
+
+    drivers_find_all = driver_repository.find_all()
+    expected_offense = [driver_1]
+
+    assert drivers_find_all == expected_offense
+
+    retrieved_driver= driver_repository.find_by_id(1)
+    assert retrieved_driver.first_name == driver_1.first_name
+    assert retrieved_driver.last_name == driver_1.last_name
+    assert retrieved_driver.registration_number == driver_1.registration_number
+
+
+def test_insert_many_speed_camera(
+        speed_camera_repository: SpeedCameraRepository,
+        speed_camera_1: SpeedCamera,
+        speed_camera_2: SpeedCamera
+) -> None:
+    speed_camera_repository.insert_many([speed_camera_1, speed_camera_2])
+
+    retrieved_speed_camera_1 = speed_camera_repository.find_by_id(1)
+    retrieved_speed_camera_2 = speed_camera_repository.find_by_id(2)
+    assert retrieved_speed_camera_1.location == speed_camera_1.location
+    assert retrieved_speed_camera_1.allowed_speed == speed_camera_1.allowed_speed
+    assert retrieved_speed_camera_2.location == speed_camera_2.location
+    assert retrieved_speed_camera_2.allowed_speed == speed_camera_2.allowed_speed
+
+def test_insert_violation(
+        driver_repository:DriverRepository,
+        speed_camera_repository: SpeedCameraRepository,
+        offense_repository: OffenseRepository,
+        violation_repository: ViolationRepository,
+        driver_1: Driver,
+        speed_camera_1: SpeedCamera,
+        offense_1: Offense
+) -> None:
+    driver_id = driver_repository.insert(driver_1)
+
+    speed_camera_id = speed_camera_repository.insert(speed_camera_1)
+
+    offense_id = offense_repository.insert(offense_1)
+
+    expected_date = '2025-10-14'
+    violation = Violation(
+        violation_date=expected_date,
+        driver_id=driver_id,
+        speed_camera_id=speed_camera_id,
+        offense_id=offense_id
+    )
+
+    violation_id = violation_repository.insert(violation)
+    assert violation_id is not None
+
+    retrieved_violation = violation_repository.find_by_id(violation_id)
+    assert retrieved_violation.violation_date == expected_date
+    assert retrieved_violation.driver_id == driver_id
+    assert retrieved_violation.speed_camera_id == speed_camera_id
+    assert retrieved_violation.offense_id == offense_id
+
+
+def test_update_driver(driver_repository: DriverRepository, driver_1: Driver, driver_2: Driver) -> None:
+    driver_id = driver_repository.insert(driver_1)
+    updated_driver = driver_2
+
+    driver_repository.update(driver_id, updated_driver)
+    retrieved_driver = driver_repository.find_by_id(driver_id)
+    assert retrieved_driver.first_name == updated_driver.first_name
+    assert retrieved_driver.registration_number == updated_driver.registration_number
+
+def test_delete_driver(driver_repository: DriverRepository, driver_1: Driver) -> None:
+    driver_id = driver_repository.insert(driver_1)
+    driver_repository.delete(driver_id)
+    retrieved_driver = driver_repository.find_by_id(driver_id)
+    assert retrieved_driver is None
+
+
+def test_insert_many_if_not_rows(driver_repository: DriverRepository, clear_database) -> None:
+    driver_repository.insert_many([])
+    retrieved_driver = driver_repository.find_all()
+
+    assert len(retrieved_driver) == 0
+
+
+def test_with_db_connection_rollback(driver_repository: DriverRepository) -> None:
+    with pytest.raises(Exception):
+        driver_repository.insert()
+
+def test_execute_sql_file(connection_manager: MySQLConnectionManager) -> None:
+    execute_sql = SqlFileExecutor(connection_manager)
+    with pytest.raises(Error):
+        execute_sql.execute_sql_file(os.path.join(os.path.dirname(__file__), '../sql/data_test.sql'))
+
+
